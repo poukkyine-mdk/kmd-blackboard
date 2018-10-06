f04802008318dabc7050f2f8016f4648a0ab3452
poukkyine-mdk <pyaephyokyaw.22121996@outlook.com>
2018-10-06 14:58:52 +1000

    courses

--- app.js
+++ app.js
@@ -1,76 +1,126 @@
 /* Connection to Cassandra */
 "use strict";
 const cassandra = require('cassandra-driver');
-var createKsQuery ="CREATE KEYSPACE kmd_blackboard WITH REPLICATION = {  'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } ;" ;
-const client = new cassandra.Client({ contactPoints: ['172.18.0.22']});
+var createKsQuery = "CREATE KEYSPACE kmd_blackboard WITH REPLICATION = {  'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } ;";
+const client = new cassandra.Client({
+  contactPoints: ['172.18.0.22']
+});
 client.connect()
-.catch(function (err) {
+  .catch(function (err) {
     console.error('There was an error when connecting', err);
     return client.shutdown();
-});
+  });
 /* ************************************************** */
 /* Require packages, set up a few things */
-var express    = require("express"),
-    app        = express(),
-bodyParser = require("body-parser"),
-session = require("express-session");
-app.use(bodyParser.urlencoded({extended:true}));
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  session = require("express-session");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 app.use(session({
-  secret:"a12341234asflk;aksdjflaskdfjdfjsdlf",
+  secret: "!@#$%^",
   resave: true,
-  saveUninitialized:true
+  saveUninitialized: true
 }))
-
 /* *************************************************** */
-/* RESTful route */
-
-app.get("/", function(req, res) {
+/* Landing */
+app.get("/", function (req, res) {
   res.render("landing");
 });
-// Login 
-app.get("/login",function(req,res){
-  if(!req.session.user){
-    res.render("login");
-  }else{
-    res.redirect("/courses")
+/* *************************************************** */
+/* login */
+app.post("/login", function (req, res) {
+var courses;
+  if (req.body.username != "") {
+    client
+      .execute("SELECT * FROM kmd_blackboard.users WHERE username='" + req.body.username + "'",
+        function (err, result) {
+          if (!err) {
+            console.log(result.rows[0].courses);
+            if (result.rowLength == 1) {
+              if (result.rows[0].username == req.body.username && result.rows[0].password == req.body.password) {
+                courses=result.rows[0].courses;
+                req.session.username = result.rows[0].username;
+                res.redirect("/courses");
+              } else {
+                req.session.ok = false;
+                res.redirect("/login")
+              }
+            } else {
+              req.session.ok = false;
+              res.redirect("/login")
+            }
+
+          } else {
+            console.log(err);
+          }
+        })
+  } else {
+    req.session.ok = false;
+    res.send("/login")
   }
-  
 })
-app.post("/login",function(req,res){
-  req.session.user=req.body.username
-  console.log(req.session.user)
-  res.redirect("/courses")
+app.get("/login", function (req, res) {
+  var ok;
+  if (req.session.ok == false) {
+    ok = req.session.ok;
+  } else {
+    ok = true;
+  }
+  res.render("login", {
+    ok: ok
+  });
 })
-// INDEX - Show all courses
-app.get("/courses", function(req, res) {
-  
-  //if there is no login session then redirect back to "/"
-if(!req.session.user){
-  res.redirect("/login")
-}else{
- res.send("YES")
-}
-
-
+/* *************************************************** */
+/**  Show all courses **/
+app.get("/courses", function (req, res) {
+  if (req.session.username) {
+    var username=req.session.username;
+    var rows;
+    client.execute("SELECT * FROM kmd_blackboard.courses_by_user WHERE username='"+username+"'",
+    function(err,result){
+        if(!err){
+          rows=result.rows;
+          console.log(result.rows)
+          console.log(rows[0].coursename)
+          res.render("courses",{rows,rows})
+        }else{
+          console.log(err);
+        }
+    })
+  } else {
+    res.redirect("/login");
+  }
 });
+/* *************************************************** */
+/**  Show one course **/ 
+app.get("/courses/:id", function (req, res) {
+  res.render("coursedetails")
+  });
+
+
+
+
 
 
 // CREATE - Adds new course to db
-app.post("/courses", function(req, res) {
-  
+app.post("/courses", function (req, res) {
+  res.redirect("/courses")
 });
 
 // NEW - Shows form to add new courses
-app.get("/courses/new", function(req, res) {
-  res.render("new.ejs"); 
+app.get("/courses/new", function (req, res) {
+  res.render("new.ejs");
 });
 
 // SHOW - Shows info about courses
-app.get("/courses/:id", function(req, res) {
- 
+app.get("/courses/:id", function (req, res) {
+res.send("hi")
 });
 
-app.listen(80, process.env.IP, function() {
+app.listen(80, process.env.IP, function () {
   console.log("Server has Started!!!");
 });
--- seed.js
+++ seed.js
@@ -3,13 +3,21 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({ contactPoints: ['172.18.0.22']});
 
 client.connect()
-// .then(()=> client.execute("CREATE KEYSPACE cycling WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } "))
+.then(()=> client.execute("CREATE KEYSPACE kmd_blackboard WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy',  'datacenter1' : 1 } "))
 .then(() => client.execute("USE kmd_blackboard"))
-// .then(() => client.execute("CREATE TABLE courses( course_code text PRIMARY KEY, course_name text, course_lecturer text    )"))
-.then(() => client.execute("CREATE TABLE users( username text PRIMARY KEY, password text)"))
+.then(() => client.execute("CREATE TABLE kmd_blackboard.users( username text PRIMARY KEY, password text)"))
+.then(() => client.execute("CREATE TABLE kmd_blackboard.courses_by_user( username text, courseCode text, courseName text, courseLecturer text, PRIMARY KEY(username,courseCode))"))
 
+.then(() => client.execute("INSERT INTO kmd_blackboard.users(username,password) VALUES ('pk','123')" ))
 
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS2341','Web Dev','Mr.T')") )
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS3241','CS Principles','Mr.G')") )
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS5341','A+','Mr.V')") )
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS6341','Programming in Large','Mr.B')") )
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS7341','Information Retrieval','Mr.C')") )
+.then(() => client.execute("INSERT INTO kmd_blackboard.courses_by_user(username,courseCode,courseName,courseLecturer) VALUES ('pk','INFS8341','Dream League','Mr.A')") )
 
+//.then(() => client.execute("DROP KEYSPACE kmd_blackboard") )
 .then(() => client.shutdown())
 .catch(function (err) {
     console.error('There was an error when connecting', err);
